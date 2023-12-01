82ec5428fb12a369f9f21f55fd2bc49df8581fbd
renan <r.jahn@hotmail.com>
2023-12-01 22:13:55 +0100

    added day_01 python solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent of Code 2023
 
-### Day 1: C++
+### Day 1: C++, Python
 
---
+++ day_01/day_01.py
@@ -0,0 +1,17 @@
+with open('input') as file:
+    text = file.readlines()
+
+calibrated_numbers = []
+for line in text:
+    num = ''.join(c for c in line if c.isdigit())
+    
+    if len(num) == 1:
+        num = num * 2
+    elif len(num) > 2:
+        num = num[0] + num[-1]
+    
+    calibrated_numbers.append(int(num))
+
+answer = sum(calibrated_numbers)
+
+print("Sum: ", answer)
